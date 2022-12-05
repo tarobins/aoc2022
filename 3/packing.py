81e5e8e2df8c4114184791071740b325a8695665
Tom Robinson <tarobins@gmail.com>
2022-12-05 04:01:07 +0000

    Factor val fn

--- 3/packing.py
+++ 3/packing.py
@@ -2,6 +2,17 @@ f = open("input.txt")
 
 sum = 0
 
+def getVal(item: str) -> int:
+    if item >= 'a' and item <= 'z':
+        val = ord(item) - ord('a') + 1
+        print(f'val: {val}')
+        return val
+    else:
+        val = ord(item) - ord('A') + 27
+        print(f'val: {val}')
+        return val
+
+
 while line := f.readline():
     print(line.strip())
     numberOfItems = len(line) // 2
@@ -13,14 +24,7 @@ while line := f.readline():
         item = line[i]
         if item in itemsInFirstPocket:
             print(f'item: {item}')
-            if item >= 'a' and item <= 'z':
-                val = ord(item) - ord('a') + 1
-                print(f'val: {val}')
-                sum += val
-            else:
-                val = ord(item) - ord('A') + 27
-                print(f'val: {val}')
-                sum += val
+            sum += getVal(item)
             break
 
 print(sum)
