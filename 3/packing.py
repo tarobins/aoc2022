1da659d7bde3a8bfa7b4dbdf2c39650c67b3f98b
Tom Robinson <tarobins@gmail.com>
2022-12-05 04:23:31 +0000

    Day 3 part 2

--- 3/packing.py
+++ 3/packing.py
@@ -5,27 +5,31 @@ sum = 0
 def getVal(item: str) -> int:
     if item >= 'a' and item <= 'z':
         val = ord(item) - ord('a') + 1
-        print(f'val: {val}')
         return val
     else:
         val = ord(item) - ord('A') + 27
-        print(f'val: {val}')
         return val
 
 
-while line := f.readline():
-    print(line.strip())
-    numberOfItems = len(line) // 2
-    itemsInFirstPocket = set()
-    for i in range(0, numberOfItems):
-        itemsInFirstPocket.add(line[i])
-    print(itemsInFirstPocket)
-    for i in range(numberOfItems, 2 * numberOfItems):
-        item = line[i]
-        if item in itemsInFirstPocket:
-            print(f'item: {item}')
-            sum += getVal(item)
-            break
+while line1 := f.readline().strip():
+    line2 = f.readline().strip()
+    line3 = f.readline().strip()
+    
+    print(line1)
+    print(line2)
+    print(line3)
+    
+    set1 = set(line1)
+    set2 = set(line2)
+    set3 = set(line3)
+
+
+    dup = list(set1.intersection(set2).intersection(set3))[0]
+
+    print(f'dup: {dup}')
+
+    sum += getVal(dup)
+
 
 print(sum)
     
