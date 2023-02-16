fd32c08921bb42d68a17a83a486a92c8499b0463
robinsontom <robinsontom@google.com>
2023-02-15 22:25:35 -0800

    Day 12 part 2

--- 12/path.py
+++ 12/path.py
@@ -1,10 +1,12 @@
 from absl import app
 
 def find_start(letters):
+    starts = []
     for row_number, row in enumerate(letters):
         for column_number, cell in enumerate(row):
-            if cell == 'S':
-                return (row_number, column_number)
+            if cell == 'S' or cell == 'a':
+                starts.append((row_number, column_number))
+    return starts
 
 def getLetterCode(letter):
     if letter == 'S':
@@ -20,42 +22,29 @@ def add_if_can_go(letters, new_position, current_letter_code, queue, visited):
     if (new_position[0], new_position[1]) in visited:
         return
     if new_letter_code <= current_letter_code + 1:
-        # print(f'  Adding {new_position}')
         visited.add((new_position[0], new_position[1]))
         queue.append(new_position)
 
-def main(argv):
-    input_file = argv[1]
-    f = open(input_file)
-
-    letters = []
-
-    while line := f.readline().strip():
-        letters.append(list(line))
-    # print(letters)
+def find_path_length(letters, start):
+    # print(f'Starting at {start}')
     height = len(letters)
     width = len(letters[0])
-
-    print(f'Width: {width} Height: {height}')
-
-    start = find_start(letters)
 
     queue = []
     queue.append(start + (0, ))
     visited = set()
 
-    # print(queue)
     count = 0
 
     while True:
+        if len(queue) == 0:
+            return 99999
         current = queue.pop(0)
-       
+        
         count += 1
-        print(f'At {current} itr {count}')
         current_letter = letters[current[0]][current[1]]
         if (current_letter == 'E'):
-            print(f'Steps: {current[2]}')
-            break
+            return current[2]
         current_letter_code = getLetterCode(current_letter)
 
         if current[1] != 0:
@@ -71,10 +60,20 @@ def main(argv):
             down = (current[0] + 1, current[1], current[2] + 1)
             add_if_can_go(letters, down, current_letter_code, queue, visited)
 
+def main(argv):
+    input_file = argv[1]
+    f = open(input_file)
+
+    letters = []
+
+    while line := f.readline().strip():
+        letters.append(list(line))
     
-    
-    
-    
+    start = find_start(letters)
+
+    steps = min(map(lambda a : find_path_length(letters, a), start))
+
+    print(f'Steps {steps}')
 
 if __name__ == '__main__':
     app.run(main)
