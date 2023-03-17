0b76258522c5f766536b20dda93fcfc2959765ff
robinsontom <robinsontom@google.com>
2023-03-17 14:30:23 -0700

    Day 18 part 1

--- 18/surface.py
+++ 18/surface.py
@@ -1,11 +1,32 @@
 from absl import app
 from math import sqrt
 
+def process_side(sides, side):
+    if side in sides:
+        sides[side] = sides[side] + 1
+    else:
+        sides[side] = 1
 
+def get_sides(box):
+    sides = []
+
+    #xy
+    sides.append(tuple(sorted(((box[0] + 1, box[1], box[2]), (box[0] + 1, box[1] + 1, box[2]), (box[0], box[1] + 1, box[2]), (box[0], box[1], box[2])))))
+    sides.append(tuple(sorted(((box[0] + 1, box[1], box[2] + 1), (box[0] + 1, box[1] + 1, box[2] + 1), (box[0], box[1] + 1, box[2] + 1), (box[0], box[1], box[2] + 1)))))
+
+    #xz
+    sides.append(tuple(sorted(((box[0] + 1, box[1], box[2]), (box[0] + 1, box[1], box[2] + 1), (box[0], box[1], box[2] + 1), (box[0], box[1], box[2])))))
+    sides.append(tuple(sorted(((box[0] + 1, box[1] + 1, box[2]), (box[0] + 1, box[1] + 1, box[2] + 1), (box[0], box[1] + 1, box[2] + 1), (box[0], box[1] + 1, box[2])))))
+
+    #yz
+    sides.append(tuple(sorted(((box[0], box[1] + 1, box[2]), (box[0], box[1] + 1, box[2] + 1), (box[0], box[1], box[2] + 1), (box[0], box[1], box[2])))))
+    sides.append(tuple(sorted(((box[0] + 1, box[1] + 1, box[2]), (box[0] + 1, box[1] + 1, box[2] + 1), (box[0] + 1, box[1], box[2] + 1), (box[0] + 1, box[1], box[2])))))
+
+    return sides
 
 def main(argv):
     if len(argv) < 2:
-        file_name = '18/test_input.txt'
+        file_name = 'test_input.txt'
     else:
         file_name = argv[1]
 
@@ -16,98 +37,46 @@ def main(argv):
     while line := f.readline().strip():
         boxes.append(tuple(map(int, line.split(','))))
 
-    face_count = 0
-    visited = set()
+    sides = {}
     
-    xs = [x for x, y, z in boxes]
-    max_x = max(xs) + 1
-    min_x = min(xs) - 1
-    ys = [y for x, y, z in boxes]
-    max_y = max(ys) + 1
-    min_y = min(ys) - 1
-    zs = [z for x, y, z in boxes]
-    max_z = max(zs) + 1 
-    min_z = min(zs) - 1
+    
 
-    start = (min_x, min_y, min_z)
+    for box in boxes:
+        # process_side(sides, (box[0] + 1, box[1], box[2]))
+        # process_side(sides, (box[0] - 1, box[1], box[2]))
+        # process_side(sides, (box[0], box[1] + 1, box[2]))
+        # process_side(sides, (box[0], box[1] - 1, box[2]))
+        # process_side(sides, (box[0], box[1], box[2] + 1))
+        # process_side(sides, (box[0], box[1], box[2] - 1))
+        # print(sides)
+        # print(box)
+        box_sides = get_sides(box)
+        # print(box_sides)
+        for box_side in box_sides:
+            # print(f'box_side {box_side}')
+            process_side(sides, box_side)
 
-    queue = [start]
+    print(len({element: count for (element, count) in sides.items() if count == 1}))
 
-    while len(queue) > 0:
-        current = queue.pop(0)
-        if current in visited:
-            continue
-        print(current)
-        visited.add(current)
-
-        # x + 1
-        x_up = (current[0] + 1, current[1], current[2])
-        if x_up in visited:
-            pass
-        elif x_up in boxes:
-            face_count += 1
-        elif x_up[0] > max_x:
-            pass
-        else:
-            queue.append(x_up)
-
-        # x - 1
-        x_down = (current[0] - 1, current[1], current[2])
-        if x_down in visited:
-            pass
-        elif x_down in boxes:
-            face_count += 1
-        elif x_down[0] < min_x:
-            pass
-        else:
-            queue.append(x_down)
-            
-        # y + 1
-        y_up = (current[0], current[1] + 1, current[2])
-        if y_up in visited:
-            pass
-        elif y_up in boxes:
-            face_count += 1
-        elif y_up[1] > max_y:
-            pass
-        else:
-            queue.append(y_up)
-
-        # y - 1
-        y_down = (current[0], current[1] - 1, current[2])
-        if y_down in visited:
-            pass
-        elif y_down in boxes:
-            face_count += 1
-        elif y_down[1] < min_y:
-            pass
-        else:
-            queue.append(y_down)
+    # single_sides = 0
+    # for box in boxes:
+    #     near_box = 0
+    #     for box2 in boxes:
+    #         if box == box2:
+    #             continue
+    #         distance = sqrt((box[0] - box2[0]) ** 2 + (box[1] - box2[1]) ** 2 + (box[2] - box2[2]) ** 2)
+    #         if distance == 1:
+    #             near_box += 1
         
-        # z + 1
-        z_up = (current[0], current[1], current[2] + 1)
-        if z_up in visited:
-            pass
-        elif z_up in boxes:
-            face_count += 1
-        elif z_up[2] > max_z:
-            pass
-        else:
-            queue.append(z_up)
+    #     single_sides += 6 - near_box
 
-        # z - 1
-        z_down = (current[0], current[1], current[2] - 1)
-        if z_down in visited:
-            pass
-        elif z_down in boxes:
-            face_count += 1
-        elif z_down[2] < min_z:
-            pass
-        else:
-            queue.append(z_down)    
+    # print(single_sides)
 
-    print(face_count)
 
+
+
+    # print(sides)
+    # print(len({element: count for (element, count) in sides.items() if count > 1}))
 
 
     
